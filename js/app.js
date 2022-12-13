cfca7e131079b83b9fd175787a8a14387e33dc65
root <sofiane.arif77@gmail.com>
2022-12-13 16:17:03 +0100

    Removed default value

--- js/app.js
+++ js/app.js
@@ -52,60 +52,60 @@ function compile() {
 
 // Default Value
 
-  const PREFIX = 'livecode-';
-  const data = ['html', 'css', 'js'].map((key) => {
-    const prefixedKey = PREFIX + key;
-    const jsonValue = localStorage.getItem(prefixedKey);
+//   const PREFIX = 'livecode-';
+//   const data = ['html', 'css', 'js'].map((key) => {
+//     const prefixedKey = PREFIX + key;
+//     const jsonValue = localStorage.getItem(prefixedKey);
 
-    if (jsonValue != null) return JSON.parse(jsonValue);
-  });
-  setInitial(data);
-  document.body.onkeyup = function () {
-    localStorage.setItem('livecode-html', JSON.stringify(html.value));
-    localStorage.setItem('livecode-css', JSON.stringify(css.value));
-    localStorage.setItem('livecode-js', JSON.stringify(js.value));
-    code.open();
-    code.writeln(
-      html.value +
-        '<style>' +
-        css.value +
-        '</style>' +
-        '<script>' +
-        js.value +
-        '</script>'
-    );
-    code.close();
-  };
+//     if (jsonValue != null) return JSON.parse(jsonValue);
+//   });
+//   setInitial(data);
+//   document.body.onkeyup = function () {
+//     localStorage.setItem('livecode-html', JSON.stringify(html.value));
+//     localStorage.setItem('livecode-css', JSON.stringify(css.value));
+//     localStorage.setItem('livecode-js', JSON.stringify(js.value));
+//     code.open();
+//     code.writeln(
+//       html.value +
+//         '<style>' +
+//         css.value +
+//         '</style>' +
+//         '<script>' +
+//         js.value +
+//         '</script>'
+//     );
+//     code.close();
+//   };
 
 
-  function setInitial(data) {
-  let htmlContent = data[0] || '<h1>Hello World</h1>';
-  let cssContent =
-    data[1] ||
-    `body {
-    background-color: #222;
-    }
-    h1 {
-      color: #fff;
-      text-align: center;
-      margin-top: 10%;
-    }`;
-  let jsContent = data[2] || `document.getElementById('h1')`;
-  css.value = cssContent;
-  js.value = jsContent;
-  html.value = htmlContent;
-  code.open();
-  code.writeln(
-    htmlContent +
-      '<style>' +
-      cssContent +
-      '</style>' +
-      '<script>' +
-      jsContent +
-      '</script>'
-  );
-  code.close();
-}
+//   function setInitial(data) {
+//   let htmlContent = data[0] || '<h1>Hello World</h1>';
+//   let cssContent =
+//     data[1] ||
+//     `body {
+//     background-color: #222;
+//     }
+//     h1 {
+//       color: #fff;
+//       text-align: center;
+//       margin-top: 10%;
+//     }`;
+//   let jsContent = data[2] || `document.getElementById('h1')`;
+//   css.value = cssContent;
+//   js.value = jsContent;
+//   html.value = htmlContent;
+//   code.open();
+//   code.writeln(
+//     htmlContent +
+//       '<style>' +
+//       cssContent +
+//       '</style>' +
+//       '<script>' +
+//       jsContent +
+//       '</script>'
+//   );
+//   code.close();
+// }
 
 
 // bouton clear
